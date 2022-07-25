40b9bacb8f46876e4c57258ae0f47788c5e29883
Alan Siqueira Maia <alan.siqueira.maia@hotmail.com>
2022-07-25 07:41:42 -0300

    fix add class UserEntityTest

--- ToolBoxDeveloper.DomainContext.MVC/Domain/Entities/UserEntity.cs
+++ ToolBoxDeveloper.DomainContext.MVC/Domain/Entities/UserEntity.cs
@@ -1,10 +1,22 @@
-﻿using ToolBoxDeveloper.DomainContext.MVC.Domain.Entities.Base;
+﻿using System;
+using ToolBoxDeveloper.DomainContext.MVC.Domain.Entities.Base;
 using ToolBoxDeveloper.DomainContext.MVC.Domain.Extensions;
 
 namespace ToolBoxDeveloper.DomainContext.MVC.Domain.Entities
 {
     public class UserEntity : BaseEntity
     {
+        public UserEntity(string email)
+        {
+            if (email.IsNullOrEmptyOrWhiteSpace())
+                throw new ArgumentNullException("Email é um campo obrigatorio");
+
+            this.Email = email;
+        }
+        internal UserEntity()
+        {
+
+        }
         public string Email { get; set; }
 
         public string Password
@@ -12,8 +24,11 @@ namespace ToolBoxDeveloper.DomainContext.MVC.Domain.Entities
             get; private set;
         }
 
-        internal void SetPassword(string password)
+        public void SetPassword(string password)
         {
+            if (password.IsNullOrEmptyOrWhiteSpace())
+                throw new ArgumentNullException("Senha é um campo obrigatorio");
+
             this.Password = password.Encrypt();
         }
     }
