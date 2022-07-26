c42cb03c3066a73f4503e50667f6673d85280271
Alan Siqueira Maia <alan.siqueira.maia@hotmail.com>
2022-07-26 07:37:46 -0300

    fix refactor cryp

--- ToolBoxDeveloper.DomainContext.MVC/Domain/Extensions/CrypExtension.cs
+++ ToolBoxDeveloper.DomainContext.MVC/Domain/Extensions/CrypExtension.cs
@@ -1,8 +1,6 @@
 ﻿using System.Globalization;
 using System.Security.Cryptography;
 using System.Text;
-using ToolBoxDeveloper.DomainContext.MVC.Domain.Settings;
-using ToolBoxDeveloper.DomainContext.MVC.Domain.ValueObjects;
 
 namespace ToolBoxDeveloper.DomainContext.MVC.Domain.Extensions
 {
@@ -11,22 +9,15 @@ namespace ToolBoxDeveloper.DomainContext.MVC.Domain.Extensions
         public static string Encrypt(this string Texto)
         {
             byte[] Hash;
-
-            using (HashAlgorithm Algoritmo = SHA256.Create())
-            {
-                Hash = Algoritmo.ComputeHash(Encoding.Unicode.GetBytes(Texto));
-            }
-
             StringBuilder Retorno = new StringBuilder();
 
+            using (HashAlgorithm Algoritmo = SHA256.Create())
+                Hash = Algoritmo.ComputeHash(Encoding.Unicode.GetBytes(Texto));
+
             foreach (byte B in Hash)
-            {
                 Retorno.AppendFormat(CultureInfo.InvariantCulture, "{0:X2}", B);
-            }
 
             return Retorno.ToString();
         }
     }
- 
-   
 }
