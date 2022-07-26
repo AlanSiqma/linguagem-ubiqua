ce54a8a7357cfd7b893cfe662e97bccb89219b53
Alan Siqueira Maia <alan.siqueira.maia@hotmail.com>
2022-07-26 08:18:10 -0300

    fix add domain context dto test

--- ToolBoxDeveloper.DomainContext.MVC/Domain/Dto/DomainContextDto.cs
+++ ToolBoxDeveloper.DomainContext.MVC/Domain/Dto/DomainContextDto.cs
@@ -1,7 +1,10 @@
-﻿namespace ToolBoxDeveloper.DomainContext.MVC.Domain.Dto
+﻿using System;
+using ToolBoxDeveloper.DomainContext.MVC.Domain.Extensions;
+
+namespace ToolBoxDeveloper.DomainContext.MVC.Domain.Dto
 {
     public class DomainContextDto
-    {   
+    {
         public string Id { get; set; }
         public string Organization { get; set; }
         public string Domain { get; set; }
@@ -10,8 +13,11 @@
         public string Description { get; set; }
         public string UserRegister { get; set; }
 
-        internal DomainContextDto SetEmail(string userRegister)
+        public DomainContextDto SetEmail(string userRegister)
         {
+            if (userRegister.IsNullOrEmptyOrWhiteSpace())
+                throw new ArgumentException("Email é um campo obrigatorio");
+
             this.UserRegister = userRegister;
             return this;
         }
