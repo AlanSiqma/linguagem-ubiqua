10f003de5a4a4f20e02697d7b851e0a257b8f7ba
Alan Siqueira Maia <alan.siqueira.maia@hotmail.com>
2022-08-10 07:37:46 -0300

    fix add proj infra

---
+++ ToolBoxDeveloper.DomainContext.Infra/Data/DomainContextRepository.cs
@@ -0,0 +1,14 @@
+﻿using ToolBoxDeveloper.DomainContext.Domain.Contracts;
+using ToolBoxDeveloper.DomainContext.Domain.Entities;
+using ToolBoxDeveloper.DomainContext.Infra.Data.Base;
+
+namespace ToolBoxDeveloper.DomainContext.Infra.Data
+{
+    public class DomainContextRepository: RepositoryBase<DomainContextEntity>, IDomainContextRepository
+    {
+        public DomainContextRepository(IDatabaseSettings settings):base(settings)
+        {
+
+        }
+    }
+}
