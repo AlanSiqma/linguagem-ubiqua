fc2b8fa47104d854c68da66074c00f4c942bff43
Alan Siqueira Maia <alan.siqueira.maia@hotmail.com>
2022-08-18 09:12:35 -0300

    fix add refactor startup

--- ToolBoxDeveloper.DomainContext.MVC/Startup.cs
+++ ToolBoxDeveloper.DomainContext.MVC/Startup.cs
@@ -1,17 +1,14 @@
-using HealthChecks.UI.Client;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Serilog;
-using ToolBoxDeveloper.DomainContext.MVC.CustomHealthChecks;
 using ToolBoxDeveloper.DomainContext.Domain.Contracts;
 using ToolBoxDeveloper.DomainContext.Domain.Settings;
 using ToolBoxDeveloper.DomainContext.IoC;
+using ToolBoxDeveloper.DomainContext.MVC.Extensions;
 
 namespace ToolBoxDeveloper.DomainContext.MVC
 {
@@ -28,44 +25,15 @@ namespace ToolBoxDeveloper.DomainContext.MVC
         {
             services.AddControllersWithViews();
 
-            services.Configure<DatabaseSettings>(
-                      Configuration.GetSection(nameof(DatabaseSettings))
-              );
+            services.Configure<DatabaseSettings>(Configuration.GetSection(nameof(DatabaseSettings)));
 
-            services.AddSingleton<IDatabaseSettings>(sp =>
-                sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+            services.AddSingleton<IDatabaseSettings>(sp =>sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
 
             services.AddInjectionConfiguration();
 
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(opt =>
-            {
-                opt.LoginPath = new PathString("/Autentication/Index");
-                opt.LogoutPath = new PathString("/Autentication/Logout");
-                opt.Cookie = new CookieBuilder()
-                {
-                    Name = "DomainContext",
-                };
-            });
+            services.AddHealthChecksConfiguration(Configuration);
 
-            var configurationSection = Configuration.GetSection(nameof(DatabaseSettings));
-            DatabaseSettings appSettings = new DatabaseSettings();
-            ConfigurationBinder.Bind(configurationSection, appSettings);
-
-            services.AddHealthChecks()
-                .AddMongoDb(mongodbConnectionString: appSettings.ConnectionString,
-                name: "Instancia mongoDB")
-                .AddCheck<DependeciesValidadeHealthCheck>("Health Checks customizavel");
-
-            services.AddHealthChecksUI(setupSettings: setup =>
-            {
-                setup.SetEvaluationTimeInSeconds(5);
-                setup.MaximumHistoryEntriesPerEndpoint(10);
-                setup.AddHealthCheckEndpoint("API com Health Checks", "/health");
-            }).AddInMemoryStorage(); ;
-
-            services.AddHealthChecksUI();
-
+            services.AddAuthenticationConfiguration();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
@@ -92,20 +60,14 @@ namespace ToolBoxDeveloper.DomainContext.MVC
                 endpoints.MapControllers();
             });
 
-            app.UseHealthChecks("/health", new HealthCheckOptions
-            {
-                Predicate = p => true,
-                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-            });
-
-            app.UseHealthChecksUI(options => { options.UIPath = "/dashboard"; });
+            app.UseHealthChecksConfiguration();
 
             app.UseEndpoints(endpoints =>
-                {
+            {
                     endpoints.MapControllerRoute(
                         name: "default",
                         pattern: "{controller=Home}/{action=Index}/{id?}");
-                });
+            });
         }
     }
 }
