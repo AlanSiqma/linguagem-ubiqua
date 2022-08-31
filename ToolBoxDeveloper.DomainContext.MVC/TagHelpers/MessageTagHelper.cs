f57b41c3e7a6fa22f50c3b498843e828d590102b
Alan Siqueira Maia <alan.siqueira.maia@hotmail.com>
2022-08-31 09:46:35 -0300

    fix add tag helper

---
+++ ToolBoxDeveloper.DomainContext.MVC/TagHelpers/MessageTagHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ToolBoxDeveloper.DomainContext.MVC.TagHelpers
+{
+    public class MessageTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetContent("Add Tag Helper");
+            //base.Process(context, output);
+        }
+    }
+}
