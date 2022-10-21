b47b9cbe4c00c8bf9e591c2fd1ecb1e444b72b0d
Alan Siqueira Maia <alan.siqueira.maia@hotmail.com>
2022-10-21 06:27:54 -0300

    fix create base contract service

--- ToolBoxDeveloper.DomainContext.Domain/Contracts/Services/IDomainContextService.cs
+++ ToolBoxDeveloper.DomainContext.Domain/Contracts/Services/IDomainContextService.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using ToolBoxDeveloper.DomainContext.Domain.Contracts.Services.Base;
 using ToolBoxDeveloper.DomainContext.Domain.Dto;
 
 namespace ToolBoxDeveloper.DomainContext.Domain.Contracts.Services
 {
-    public interface IDomainContextService
+    public interface IDomainContextService:IBaseService<DomainContextDto>
     {
-        Task<List<DomainContextDto>> GetAll();
-        Task AddOrUpdate(DomainContextDto dto);
-        Task<DomainContextDto> Find(string id);
-        Task Delete(string id);
     }
 }
