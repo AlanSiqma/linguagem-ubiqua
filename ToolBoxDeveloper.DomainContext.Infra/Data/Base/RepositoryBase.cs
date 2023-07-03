9155a2bca51001528915ace3834fd956aafe8622
Alan Siqueira Maia <alan.siqma@gmail.com>
2023-07-03 09:52:55 -0300

    fix update base

--- ToolBoxDeveloper.DomainContext.Infra/Data/Base/RepositoryBase.cs
+++ ToolBoxDeveloper.DomainContext.Infra/Data/Base/RepositoryBase.cs
@@ -12,66 +12,51 @@ namespace ToolBoxDeveloper.DomainContext.Infra.Data.Base
 {
     public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : BaseEntity
     {
-        public IMongoCollection<TEntity> _collections;
+        private readonly IMongoCollection<TEntity> _collection;
 
-        private string _dataBaseName = typeof(TEntity).Name;
         public RepositoryBase(IMongoDatabase database)
         {
-            this._collections = database.GetCollection<TEntity>(_dataBaseName);
+            string collectionName = typeof(TEntity).Name;
+            _collection = database.GetCollection<TEntity>(collectionName);
         }
 
         public async Task<List<TEntity>> Get()
         {
-            var list = await this._collections.FindAsync(x => true);
+            var list = await _collection.FindAsync(x => true);
             return list.ToList();
         }
+
         public async Task<List<TEntity>> Get(Expression<Func<TEntity, bool>> predicate)
         {
-            var list = await this._collections.FindAsync(predicate);
+            var list = await _collection.FindAsync(predicate);
             return list.ToList();
         }
 
         public async Task<TEntity> Get(string id)
         {
-            var entity = await this._collections.FindAsync<TEntity>(RepositorySpec.FindEntityById<TEntity>(id));
+            var entity = await _collection.FindAsync<TEntity>(RepositorySpec.FindEntityById<TEntity>(id));
             return entity.FirstOrDefault();
         }
 
         public async Task<TEntity> Create(TEntity entity)
         {
-            await _collections.InsertOneAsync(entity);
+            await _collection.InsertOneAsync(entity);
             return entity;
         }
 
         public async Task Update(string id, TEntity entity)
         {
-            await _collections.ReplaceOneAsync(RepositorySpec.FindEntityById<TEntity>(id), entity);
+            await _collection.ReplaceOneAsync(RepositorySpec.FindEntityById<TEntity>(id), entity);
         }
 
         public async Task Remove(TEntity entity)
         {
-            await _collections.DeleteOneAsync(RepositorySpec.FindEntityById<TEntity>(entity.Id));
+            await _collection.DeleteOneAsync(RepositorySpec.FindEntityById<TEntity>(entity.Id));
         }
 
         public void Dispose()
         {
-            try
-            {
-
-                if (_collections == null)
-                {
-                    GC.SuppressFinalize(this);
-                }
-                else
-                {
-                    _collections = null;
-                    _dataBaseName = null;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new ApplicationException(ex.Message);
-            }
+            GC.SuppressFinalize(this);
         }
     }
 }
